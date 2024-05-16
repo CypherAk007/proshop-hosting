db66ce53ba07280d2fc37f8e7e0ede6a76edebe7
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-05-16 20:36:11 +0530

    dirs test

--- backend/settings.py
+++ backend/settings.py
@@ -116,9 +116,9 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        
+        'DIRS':['frontend/build'],
         'DIRS': [
-            os.path.join(BASE_DIR,'frontend/build/index.html')
+            os.path.join(BASE_DIR,'frontend/build')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -190,7 +190,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 # Default primary key field type
@@ -200,12 +200,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATICFILES_DIRS = [
+    BASE_DIR / 'frontend/build',
     BASE_DIR / 'static',
     BASE_DIR / 'frontend/build/static'
 ]
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
 
 CORS_ALLOW_ALL_ORIGINS =True
 
