a39a1ae0dcf8f6530165d6e5d8d645df7e7ea67e
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-05-16 20:21:38 +0530

    dirs test

--- backend/settings.py
+++ backend/settings.py
@@ -116,6 +116,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS':['frontend/build'],
         'DIRS': [
             os.path.join(BASE_DIR,'frontend/build')
         ],
